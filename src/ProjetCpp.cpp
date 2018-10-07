9c6288a2884ce7633571a54c877c8f9754127345
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-10-07 19:38:32 -0400

    Création de tableaux de personnages et armes

--- src/ProjetCpp.cpp
+++ src/ProjetCpp.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "Archer.h"
 #include "Bow.h"
@@ -21,20 +22,32 @@
 using namespace std;
 
 int main() {
+	vector<Weapon*> weaponsList;
+	vector<Character*> charactersList;
+
+	// Weapon creation
 	Weapon* bow = new Bow();
 	Weapon* spell = new Spell();
 	Weapon* staff = new Staff();
 
+	weaponsList.push_back(bow);
+	weaponsList.push_back(spell);
+	weaponsList.push_back(staff);
+
+	// Character creation
 	Character* archer = new Archer(*bow);
 	Character* mage = new Mage(*spell);
 	Character* priestess = new Priestess(*staff);
 
+	charactersList.push_back(archer);
+	charactersList.push_back(mage);
+	charactersList.push_back(priestess);
+
+	// Program
 	priestess->useWeapon(*archer);
 	cout << "Archer current HP : " << archer->hitPoints << endl;
-
 	mage->useWeapon(*archer);
 	mage->useWeapon(*archer);
-
 	cout << "Archer current HP : " << archer->hitPoints << endl;
 
 	// Data export
@@ -42,22 +55,22 @@ int main() {
 	worldFile.open("data/world.xml");
 		worldFile << "<World>" << endl;
 			worldFile << "<Character>" << endl;
-				worldFile << archer->exportDatas() << endl;
-				worldFile << mage->exportDatas() << endl;
-				worldFile << priestess->exportDatas() << endl;
+				for(Character* character : charactersList) {
+					worldFile << character->exportDatas() << endl;
+				}
 			worldFile << "</Character>" << endl;
 			worldFile << "<Spell>" << endl;
-				worldFile << bow->exportDatas() << endl;
-				worldFile << spell->exportDatas() << endl;
-				worldFile << staff->exportDatas() << endl;
+				for(Weapon* weapon : weaponsList) {
+					worldFile << weapon->exportDatas() << endl;
+				}
 			worldFile << "</Spell>" << endl;
-		worldFile << "</World>" << endl;
+		worldFile << "</World>";
 	worldFile.close();
 
+	//deletion
 	delete archer;
 	delete mage;
 	delete priestess;
-
 	delete bow;
 	delete spell;
 	delete staff;
